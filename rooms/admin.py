91d25ce2d8bd0df8399404a3365b76a28fa222dc
Somraj Chowdhury <chowdhury.somraj08@gmail.com>
2025-02-17 21:21:13 +0530

    Created the Photo model to add room images

--- rooms/admin.py
+++ rooms/admin.py
@@ -14,3 +14,31 @@ class RoomTypeAdmin(admin.ModelAdmin):
     """Admin class for RoomType model."""
 
     pass
+
+
+@admin.register(models.Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+    """Admin class for Amenity model."""
+
+    pass
+
+
+@admin.register(models.Facility)
+class FacilityAdmin(admin.ModelAdmin):
+    """Admin class for Facility model."""
+
+    pass
+
+
+@admin.register(models.HouseRule)
+class HouseRuleAdmin(admin.ModelAdmin):
+    """Admin class for HouseRule model."""
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """Admin class for Photo model."""
+
+    pass
--- rooms/models.py
+++ rooms/models.py
@@ -7,7 +7,29 @@ from users.models import User
 class RoomType(AbstractItem):
     """RoomType model definition."""
 
-    pass
+    class Meta:
+        verbose_name = 'Room Type'
+
+
+class Amenity(AbstractItem):
+    """Amenity model definition."""
+
+    class Meta:
+        verbose_name_plural = 'Amenities'
+
+
+class Facility(AbstractItem):
+    """Facility model definition."""
+
+    class Meta:
+        verbose_name_plural = 'Facilities'
+
+
+class HouseRule(AbstractItem):
+    """HouseRule model definition."""
+
+    class Meta:
+        verbose_name = 'House Rule'
 
 
 class Room(AbstractDateTime):
@@ -27,7 +49,22 @@ class Room(AbstractDateTime):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    room_type = models.ManyToManyField(to=RoomType, blank=True)
+    room_type = models.ForeignKey(to=RoomType, on_delete=models.SET_NULL,
+                                  null=True)
+    amenities = models.ManyToManyField(to=Amenity, blank=True)
+    facilities = models.ManyToManyField(to=Facility, blank=True)
+    house_rules = models.ManyToManyField(to=HouseRule, blank=True)
 
     def __str__(self):
         return self.name
+
+
+class Photo(AbstractDateTime):
+    """Photo model definition."""
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey(to=Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
