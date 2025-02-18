a722c740d6363d3f08990e7b56ffcd74deb9b92d
Somraj Chowdhury <chowdhury.somraj08@gmail.com>
2025-02-18 08:57:25 +0530

    Created Review model

---
+++ reviews/models.py
@@ -0,0 +1,23 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+from core.models import AbstractDateTime
+from rooms.models import Room
+from users.models import User
+
+
+class Review(AbstractDateTime):
+    """Review model definition."""
+
+    review = models.TextField()
+    cleanliness = models.PositiveSmallIntegerField()
+    accuracy = models.PositiveSmallIntegerField()
+    check_in = models.PositiveSmallIntegerField()
+    communication = models.PositiveSmallIntegerField()
+    location = models.PositiveSmallIntegerField()
+    value = models.PositiveSmallIntegerField()
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    room = models.ForeignKey(to=Room, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.review} - {self.room}"
